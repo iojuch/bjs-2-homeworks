d044dc8cb4b5f89ceac35d8555ccc29eaa42a779
Александр Забелинский <petrovich8384@mail.ru>
2024-06-14 23:13:27 +0300

    Arrays aded

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,22 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length !== arr2.length) {
+		return false
+	}
+
+	return arr1.every((value, index) => {
+		console.log(value, arr2[index])
+		return value === arr2[index]
+	})
+
 }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
+function getUsersNamesInAgeRange(user, gender) {
+	const filteredUser = user.filter(user => user.gender === gender);
+	if (filteredUser.length === 0) {
+		return 0;
+	}
+	const totalAge = filteredUser.reduce((acc, user) => acc + user.age, 0);
+
+	const averageAge = totalAge / filteredUser.length;
+	return averageAge;
 }
